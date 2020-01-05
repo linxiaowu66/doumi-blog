b5343ca02812f0a1d438a85dbfda2836a25e0fe5
linxiaowu66 <linguang661990@126.com>
2020-01-05 11:26:25 +0800

    feat(doumi): others page

---
+++ src/browser/doumi.view.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View ***REMOVED*** from '@malagu/react/lib/browser';
+
+interface Prop {***REMOVED***
+interface State {
+  response: string
+***REMOVED***
+
+@View('/about/doumi')
+export default class AboutDouMi extends React.Component<Prop, State> {
+***REMOVED***
