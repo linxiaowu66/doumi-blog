74708c3dba810f320189b39eb008806b3904208f
linxiaowu66 <linguang661990@126.com>
2020-01-06 15:47:59 +0800

    feat(about): finish pages

---
+++ src/browser/components/clickTooltip.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Tooltip, Fab } from '@material-ui/core';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+
+interface ClickTooltipProps {
+  title: string;
+  children: React.ReactElement;
+  extraAction?: (extra: string) => void;
+}
+
+export default function ClickTooltip(props: ClickTooltipProps) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+    if (props.extraAction) {
+      props.extraAction(props.title)
+    }
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+    if (props.extraAction) {
+      props.extraAction(props.title);
+    }
+  };
+
+  return (<ClickAwayListener onClickAway={handleTooltipClose}>
+      <div>
+        <Tooltip
+          PopperProps={{
+            disablePortal: true,
+          }}
+          onClose={handleTooltipClose}
+          open={open}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          title={props.title}
+        >
+          <Fab onClick={handleTooltipOpen} color="secondary">
+            {
+              props.children
+            }
+          </Fab>
+        </Tooltip>
+      </div>
+    </ClickAwayListener>
+  )
+}
