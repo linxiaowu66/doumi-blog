8146f771af22f4f594f0c5e5e19e36a375e2cb2d
linxiaowu66 <linguang661990@126.com>
2020-03-10 21:45:00 +0800

    fix(winston): bug for format timestamp

--- src/node/configs/logger.config.ts
+++ src/node/configs/logger.config.ts
@@ -2,7 +2,6 @@ import { Component, Value, LOGGER_CONFIG } from '@malagu/core';
 import { WinstonConfig } from 'malagu-winston';
 import { format, transports } from 'winston';
 import * as Transport from 'winston-transport';
-import { format as formatDate } from 'date-fns';
 
 const DailyRotateFile = require('winston-daily-rotate-file');
 
@@ -21,7 +20,7 @@ export class WinstonConfigImpl implements WinstonConfig {
       new DailyRotateFile({
         ...dailyRotateConfig,
         format: format.combine(
-          format.timestamp({ format: formatDate(new Date(), 'yyyy-MM-dd HH:mm:ss,SSS')}),
+          format.timestamp({ format: 'yyyy-MM-dd HH:mm:ss,SSS'}),
           format.simple(),
           format.printf(msg =>
             `${msg.timestamp} - ${msg.level}: ${msg.message}`
@@ -33,7 +32,7 @@ export class WinstonConfigImpl implements WinstonConfig {
       this.transports.push(new transports.Console({
         format: format.combine(
           format.colorize(),
-          format.timestamp({ format: formatDate(new Date(), 'yyyy-MM-dd HH:mm:ss,SSS')}),
+          format.timestamp({ format: 'yyyy-MM-dd HH:mm:ss,SSS'}),
           format.simple(),
           format.printf(msg =>
             `${msg.timestamp} - ${msg.level}: ${msg.message}`
